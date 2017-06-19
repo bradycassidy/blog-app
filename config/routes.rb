38badc12aceccc1262c3a38a9184219d48e3a23f
Brady Cassidy <bradymcassidy@gmail.com>
2017-06-18 22:37:07 -0600

    set up Pages resource excluding database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
+  get '/pages', to: 'pages#index'   ## adds pages route to index method of pages controller.
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
