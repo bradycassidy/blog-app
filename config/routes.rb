86eb17ec01ede151a6dc1e0f7e167db9cae6a11a
Brady Cassidy <bradymcassidy@gmail.com>
2017-06-19 23:59:41 -0600

    Created Pages paths, show method in controller and show view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
-  get '/pages', to: 'pages#index'   ## adds pages route to index method of pages controller.
+  get '/pages', to: 'pages#index'
+  get '/pages/:id', to: 'pages#show', as: 'page'
+
+  ## adds pages route to index method of pages controller.
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
