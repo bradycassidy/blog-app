7c832dd239bc6f0560d829c5be25e2ea1233c380
Brady Cassidy <bradymcassidy@gmail.com>
2017-07-01 23:58:35 -0600

    Created create_user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619043757) do
+ActiveRecord::Schema.define(version: 20170702054828) do
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20170619043757) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "body"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
